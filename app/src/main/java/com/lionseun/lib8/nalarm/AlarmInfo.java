a810efdba8aed4acc7285d38713b481a8f5454d3
Lionseun Ling <lionseun@gmail.com>
2017-05-31 23:26:57 +0800

    add menu's settings SettingsActivity AlarmInfo

---
+++ app/src/main/java/com/lionseun/lib8/nalarm/AlarmInfo.java
@@ -0,0 +1,13 @@
+package com.lionseun.lib8.nalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_info);
+    }
+}
--- app/src/main/java/com/lionseun/lib8/nalarm/MainActivity.java
+++ app/src/main/java/com/lionseun/lib8/nalarm/MainActivity.java
@@ -1,13 +1,63 @@
 package com.lionseun.lib8.nalarm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    private Toolbar toolbar;
+
+    @BindView(R.id.alarm_list)
+    private ListView mAlarmList;
+
+    @BindView(R.id.alarm_add)
+    private FloatingActionButton mAlarmAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        mAlarmAdd.setOnClickListener((v)-> {
+
+        });
+
+        mAlarmList.setAdapter(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "完成设置", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
